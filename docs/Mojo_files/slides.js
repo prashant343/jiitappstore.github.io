46da8d4fb5ddff3891c326e9bb68a07e835208cb
prashant kaushik <prashant@ovalelabs.com>
2017-01-25 20:27:03 +0530

    second test

---
+++ docs/Mojo_files/slides.js
@@ -0,0 +1,25 @@
+var mojoSlides = [
+  {
+    id: 'slide0',
+    content: '<center><br /><br /><br /><span style="font-weight:200;font-size:72px;">Hello</span><br /><br /><br /><br /><br /><br /><span style="font-size:18px">Swipe to the left to get started!</span></center>'
+  },
+  {
+    id: 'slide1',
+    picture: '<div class=""><img src="img/welcome-frames/mojo-sources.png" alt="Sources" style="width:55%" /></div>',
+    text: 'Mojo Installer is repository-based. That means that you host the content! Unlike the App Store, you can create and host your own Mojo repository with your favorite apps and games.<br /><br /><small>(Repositories are in JSON format; more information can be found on the Mojo website.)</small>'
+  },
+  {
+    id: 'slide2',
+    picture: '<div class=""><img src="img/welcome-frames/mojo-history.png" alt="History" style="width:55%" /></div>',
+    text: 'But just because you host the content doesn\'t mean we can\'t keep track! Mojo also stores your download history for you, like the App Store, so you never have to.<br /><br /><small>(All history is stored client-side and <strong>not</strong> on Mojo\'s servers.)</small>'
+  },
+  {
+    id: 'slide3',
+    picture: '<div class=""><img src="img/welcome-frames/mojo-settings.png" alt="Settings" style="width:55%" /></div>',
+    text: 'Mojo Installer is also (slightly) color customizable, has a built-in updater, and runs even when your device is offline; all unique features that put us ahead of our competition!<br /><br /><small>(<strong>Most</strong> of the listed features are not Mojo-exclusive, but some of them are! ;)</small>'
+  },
+  {
+    id: 'slide4',
+    content: '<center><br /><br /><br /><span style="font-weight:200;font-size:42px;">Welcome to Mojo</span><br /><br /><br /><br /><br /><br /><br /><br /><a class="close-welcomescreen" style="color:white!important;font-size:22px;" href="#">Get Started</a></center>'
+  }
+];
